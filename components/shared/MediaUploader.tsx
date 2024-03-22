fe879957acbf824f95d6759c2f0feea4286c5270
theatulgupta <atulgupta3058@gmail.com>
2024-03-22 12:55:43 +0530

    Implement Clerk Webhooks

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useToast } from '../ui/use-toast';
+
+const MediaUploader = () => {
+  const toast = useToast();
+  return <div>Media Uploader</div>;
+};
+
+export default MediaUploader;
